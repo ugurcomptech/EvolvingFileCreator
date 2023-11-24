411fb7e88c9e5d4bfb6f069d89642748173456ce
Uğur <133202238+ugurcomptech@users.noreply.github.com>
2023-11-25 02:34:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,50 @@
 import os
 import time
+import logging
 import shutil
 
-def create_file(file_path):
-    """
-    Belirtilen dosya yolunda yeni bir dosya oluşturur ve içine boş bir metin yazar.
+logging.basicConfig(level=logging.INFO)
 
-    Args:
-        file_path (str): Oluşturulacak dosya yolunu içeren dizgi.
-    """
-    with open(file_path, "w") as file:
-        file.write("")
+def create_file(file_path):
+    try:
+        with open(file_path, "x"):
+            pass
+    except FileExistsError:
+        pass
 
 def increase_file_size(file_path, size_mb):
-    """
-    Belirtilen dosyanın boyutunu artırır.
+    with open(file_path, "a") as file:
+        file.write("\0" * (size_mb * 1024 * 1024))
 
-    Args:
-        file_path (str): Boyutu artırılacak dosyanın yolu.
-        size_mb (int): Dosyanın artırılacak boyutu megabayt cinsinden.
-    """
-    with open(file_path, "ab+") as file:
-        file.seek(0, os.SEEK_END)
-        file.write(b"\0" * (size_mb * 1024 * 1024))
+def copy_files(source_path, target_path, file_count):
+    for i in range(1, file_count):
+        source_file = os.path.join(source_path, f"ArtanBoyutluDosya_{i}.txt")
+        target_file = os.path.join(target_path, f"ArtanBoyutluDosya_{i + 1}.txt")
 
-def main():
-    """
-    Ana işlemi yürüten fonksiyon.
-    """
-    folder_paths = ["G:\\Program2\\1", "G:\\Program1\\2"]  
-    file_count = 5
-    file_size_increase = 500  
-    max_loop_count = 100  
+        try:
+            shutil.copy(source_file, target_file)
+        except FileNotFoundError:
+            pass
 
-    # Klasörleri oluştur ve dosyaları başlangıç boyutuyla oluştur
+def main(file_count=1, file_size_increase=500, max_loop_count=100):
+    folder_paths = ["F:\\Script2\\2"]
+    
     for folder_path in folder_paths:
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path, exist_ok=True)
-
+        os.makedirs(folder_path, exist_ok=True)
         for i in range(file_count):
             file_path = os.path.join(folder_path, f"ArtanBoyutluDosya_{i + 1}.txt")
             create_file(file_path)
 
     loop_count = 0
     while loop_count < max_loop_count:
-        # Dosyaların boyutunu artır ve birbirine kopyala
         for folder_path in folder_paths:
             for i in range(file_count):
                 file_path = os.path.join(folder_path, f"ArtanBoyutluDosya_{i + 1}.txt")
                 increase_file_size(file_path, file_size_increase)
 
-        for folder_path in folder_paths:
-            for i in range(1, file_count):
-                source_file = os.path.join(folder_path, f"ArtanBoyutluDosya_{i}.txt")
-                target_file = os.path.join(folder_path, f"ArtanBoyutluDosya_{i + 1}.txt")
-                shutil.copyfile(source_file, target_file)
+            copy_files(folder_path, folder_path, file_count)
 
+        logging.info(f"Loop {loop_count + 1}: Files processed.")
         time.sleep(1)
         loop_count += 1
 
